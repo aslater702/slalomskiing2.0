9d21ffcd028645dbe55026783d80669c1bde31bc
Aslater702 <audrey.slater@slalom.com>
2023-05-22 16:58:57 -0700

    update us-east-1 outputs

--- us-east-1/outputs.tf
+++ us-east-1/outputs.tf
@@ -2,3 +2,22 @@
 output "vpc_id" {
     value = module.vpc.vpc_id
 }
+output "s3_bucket_name" {
+  value = aws_s3_bucket.slalom_skiing_primary_bucket.id
+}
+
+output "s3_domain_name" {
+  value = aws_s3_bucket_website_configuration.slalom_skiing_website.website_domain
+}
+
+output "s3_domain" {
+  value = aws_s3_bucket.slalom_skiing_primary.bucket_regional_domain_name
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.slalom_skiing_primary_bucket.arn
+}
+
+output "cloudfront_origin"  {
+  value = aws_cloudfront_origin_access_identity.s3_origin_identity.cloudfront_access_identity_path
+}
